ff1e4e0a280603601ccb399c632879c860c0960c
yuhyunjae12 <yuhyunjae12@naver.com>
2020-02-13 15:50:58 +0900

    queryString 추가

--- react-route/src/pages/Board.js
+++ react-route/src/pages/Board.js
@@ -1,9 +1,27 @@
 import React from 'react';
+//query string import
+import queryString from 'query-string';
 
-const Board = ({match}) => {
+
+// Ex. (params)
+// const Board = ({match}) => {
+//     return (
+//         <div>
+//             Board BoardNo : {match.params.boardNo}
+//         </div>
+//     );
+// };
+
+const Board = ({location, match}) => {
+    const query = queryString.parse(location.search);
+    console.log(query);
+
+    const no = query.no == 1;
+
     return (
         <div>
             Board BoardNo : {match.params.boardNo}
+            query : {no && 'no:queryTest'}
         </div>
     );
 };
