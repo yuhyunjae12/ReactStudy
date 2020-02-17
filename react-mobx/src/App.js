429e8be1dfb19a50ea7f7bfc0ad40c9a401d3fa4
yuhyunjae12 <yuhyunjae12@naver.com>
2020-02-17 17:55:40 +0900

    커밋

--- react-mobx/src/App.js
+++ react-mobx/src/App.js
@@ -13,3 +13,5 @@ export default class App extends Component {
     )
   }
 }
+
+//https://velog.io/@velopert/MobX-3-%EC%8B%AC%ED%99%94%EC%A0%81%EC%9D%B8-%EC%82%AC%EC%9A%A9-%EB%B0%8F-%EC%B5%9C%EC%A0%81%ED%99%94-%EB%B0%A9%EB%B2%95-tnjltay61n
