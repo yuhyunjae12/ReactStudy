18642adb0a53c4452f9e31c69afffc4a29cd8953
yuhyunjae12 <yuhyunjae12@naver.com>
2020-02-18 10:33:38 +0900

    mobx 수정 커밋

--- react-mobx/src/App.js
+++ react-mobx/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import Counter from './components/Counter'
+import SuperMarket from './components/SuperMarket'
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Counter/>
+        <hr/>
+        <SuperMarket/>
       </div>
     )
   }
